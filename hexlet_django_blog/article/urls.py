dbed59da95dae36fea39b76cc3345a306ad27ad2
saintodes <odes36@gmail.com>
2023-11-06 15:01:51 +0700

    Lesson 8 reverse

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article.views import IndexView, redirect_to_article
 
-from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path("", IndexView.as_view()),
+    path("articles/<str:tags>/<int:article_id>", IndexView.as_view(), name="article"),
+    path("", redirect_to_article, name="home"),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views import View
+from django.http import HttpResponse
 
 
 class IndexView(View):
-    def get(sel, request, *args, **kwargs):
-        return render(
-            request,
-            "articles/index.html",
-            context={
-                "app_name": "Hexlet Django Blog",
-            },
-        )
+    def get(self, request, tags, article_id, *args, **kwargs):
+        return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
+
+
+def redirect_to_article(request):
+    article_url = reverse("article", kwargs={"tags": "python", "article_id": 42})
+    return redirect(article_url)
