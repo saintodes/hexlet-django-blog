69fe4fcc48e6f99e1515d53fe0a0574e229eab33
Egor Efimov <odes36@gmail.com>
2023-11-17 18:23:20 +0700

    Lessin 18 CRUD Delete

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -68,3 +68,12 @@ class ArticleFormEditView(View):
             return redirect('articles')
 
         return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
+    
+class ArticleFormDeleteView(View):
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+        return redirect('articles')
